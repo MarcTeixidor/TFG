a982214d6e724dcf47271e2713840e9a86bae63a
MarcTeixidor <marc.teixidor03@estudiant.upf.edu>
2021-09-29 15:48:51 +0200

    added cross validation in training

--- src/data.py
+++ src/data.py
@@ -9,6 +9,19 @@ from sklearn.model_selection import train_test_split
 INPUT_PATH = "/data/"
 TRAIN_FOLDER_PATH = 'training/'
 
+class PlaylistDataset(Dataset):
+
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+    def __len__(self):
+        return len(self.x)
+
+    def __getitem__(self, idx):
+        return (self.x[idx], self.y[idx])
+
+
 class DataGenerator(object):
 
     def __init__(self, playlist_data, config):
